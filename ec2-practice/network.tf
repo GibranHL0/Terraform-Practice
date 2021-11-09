9605a7da3339553222513f9d4ae1202b75f4ba77
Gibran Herrera <gibranhl2@gmail.com>
2021-11-09 11:48:02 -0600

    Add network interface

--- ec2-practice/network.tf
+++ ec2-practice/network.tf
@@ -1,13 +1,24 @@
-resource "aws_vpc" "test_vpc" {
-  cidr_block = "148.2.0.0/16"
+resource "aws_vpc" "red_hat_vpc" {
+  cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support = true
+
   tags = {
-    Name = "assignment-devops"
+    Name = "VPC Red Hat Instances"
   }
 }
 
-resource "aws_eip" "test-elastic-ip" {
-  instance = aws_instance.test_instance.id
+resource "aws_network_interface" "red_hat_nic" {
+  subnet_id = aws_subnet.subnet_1.id
+  private_ips = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_ssh.id]
+}
+
+resource "aws_eip" "red_hat_eip" {
   vpc = true
+  network_interface = aws_network_interface.red_hat_nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on = [
+    aws_internet_gateway.red_hat_ig
+  ]
 }
--- ec2-practice/servers.tf
+++ ec2-practice/servers.tf
@@ -1,9 +1,13 @@
-resource "aws_instance" "test_instance" {
+resource "aws_instance" "red_hat_server" {
   ami           = "ami-0ba62214afa52bec7"
   instance_type = "t2.micro"
+  availability_zone = "us-east-2a"
   key_name = "my-first-server"
-  security_groups = [ "${aws_security_group.test_security_group.id}" ]
-  subnet_id = aws_subnet.test_subnet.id
+  
+  network_interface {
+    device_index = 0
+    network_interface_id = aws_network_interface.red_hat_nic.id
+  }
 
   tags = {
       Name = "Red Hat AMI"
